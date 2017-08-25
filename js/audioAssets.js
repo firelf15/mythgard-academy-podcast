300aa505e974b2397b8e548d3956a5e06136763a
Monika Spielman <firelf@lothlorien.local>
2017-08-25 10:02:24 -0700

    updated paths

--- js/audioAssets.js
+++ js/audioAssets.js
@@ -3,12 +3,12 @@
 // Audio Source - Mythgard Academy Podcast : http://media.signumuniversity.org/mythgardacademy/feed
 var audioData = [
     {
-        'title' : 'MSA001: The Fellowship of the Ring, Lecture 1',
-        'url' : 'https://s3.amazonaws.com/media.mythgardacademy/MSA001_LOTRI/MSA001_Olsen_2012-07-11_Class01_PartI.mp3'
-    },
-    {
         'title' : 'MSA001: The Fellowship of the Ring, Lecture 2',
         'url' : 'https://s3.amazonaws.com/media.mythgardacademy/MSA001_LOTRI/MSA001_Olsen_2012-07-12_Class02_PartII.mp3'
+    },
+    {
+        'title' : 'MSA001: The Fellowship of the Ring, Lecture 1',
+        'url' : 'https://s3.amazonaws.com/media.mythgardacademy/MSA001_LOTRI/MSA001_Olsen_2012-07-11_Class01_PartI.mp3'
     }
 ];
 
